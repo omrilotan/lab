5f7d80807afff81d9a5c23873e20188dbc47006e
omrilotan <omrilotan@hotmail.com>
2023-11-10 12:12:50 +0000

    [gerbil] more fixes

--- habitats/gerbil/routes/data/index.js
+++ habitats/gerbil/routes/data/index.js
@@ -1,23 +1,21 @@
 export const routes = ["/data", "/data2"].map((prefix) => ({
-	path: `${prefix}/:id`,
+	path: `${prefix}/:file`,
 	method: "get",
 	handler: async function (request, response, next) {
 		response.set("Content-Type", "application/json");
-		if (request.params.id.includes("private")) {
+		const { file } = request.params;
+		if (file.includes("private")) {
 			response.set("Cache-Control", "private");
-		}
-		if (request.params.id.includes("no-store")) {
+		} else if (file.includes("no-store")) {
 			response.set("Cache-Control", "private, no-store");
-		}
-		if (request.params.id.includes("no-cache")) {
+		} else if (file.includes("no-cache")) {
 			response.set("Cache-Control", "private, no-cache");
-		}
-		if (request.params.id.includes("cache")) {
+		} else if (file.includes("cache")) {
 			response.set("Cache-Control", "public, max-age=3600");
-		}
-		if (request.params.id.includes("client")) {
+		} else if (file.includes("client")) {
 			response.set("Cache-Control", "private, max-age=3600");
 		}
-		response.send(JSON.stringify({ id: request.params.id }));
+		response.set("file", file);
+		response.send(JSON.stringify({ file }));
 	},
 }));
